4b1d03f34552ae225355302db017bac148b20acb
Juan Navarro <jnavcamacho@gmail.com>
2018-09-20 15:52:39 -0600

    add,  side menu

---
+++ app/src/main/java/com/soa4id/tec/jnavarro/sportec/DrawerHeader.java
@@ -0,0 +1,28 @@
+package com.soa4id.tec.jnavarro.sportec;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.NonReusable;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+
+@NonReusable
+@Layout(R.layout.drawer_header)
+public class DrawerHeader {
+    @View(R.id.profileImageView)
+    private ImageView profileImage;
+
+    @View(R.id.nameTxt)
+    private TextView nameTxt;
+
+    @View(R.id.emailTxt)
+    private TextView emailTxt;
+
+    @Resolve
+    private void onResolved() {
+        nameTxt.setText("Soa User");
+        emailTxt.setText("soauser@mail.com");
+    }
+}
--- app/src/main/java/com/soa4id/tec/jnavarro/sportec/MainActivity.java
+++ app/src/main/java/com/soa4id/tec/jnavarro/sportec/MainActivity.java
@@ -1,28 +1,76 @@
 package com.soa4id.tec.jnavarro.sportec;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+
+import com.mindorks.placeholderview.PlaceHolderView;
 
 import storage.DBHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private PlaceHolderView mDrawerView;
+    private DrawerLayout mDrawer;
+    private Toolbar mToolbar;
+    private PlaceHolderView mGalleryView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.AppTheme_Credentials);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawer = findViewById(R.id.drawerLayout);
+        mDrawerView = findViewById(R.id.drawerView);
+        mToolbar = findViewById(R.id.toolbar);
+        mGalleryView = findViewById(R.id.galleryView);
+        setupDrawer();
 
         checkUserLogged();
 
 
     }
 
+    private void setupDrawer(){
+        mDrawerView
+                .addView(new DrawerHeader())
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_PROFILE))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_REQUESTS))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_MESSAGE))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_GROUPS))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_NOTIFICATIONS))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_TERMS))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_SETTINGS))
+                .addView(new DrawerMenuItem(this.getApplicationContext(), DrawerMenuItem.DRAWER_MENU_ITEM_LOGOUT));
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
+                                                                              mDrawer,
+                                                                              mToolbar,
+                                                                              R.string.open_drawer,
+                                                                              R.string.close_drawer){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawer.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+    }
+
     /**
-     *
+     *Checks if the user is logged
      */
     private void checkUserLogged () {
         DBHelper helper = new DBHelper(MainActivity.this);
