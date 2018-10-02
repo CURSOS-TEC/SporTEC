419ab8fde426b4a01e1acfdd35c8e8f844815a2f
Juan Navarro <jnavcamacho@gmail.com>
2018-10-01 21:05:13 -0600

    add, api to login

--- app/src/main/java/network/API.java
+++ app/src/main/java/network/API.java
@@ -2,7 +2,7 @@ package network;
 
 public class API {
     public static String API_BASE = "https://murmuring-fjord-87445.herokuapp.com/api";
-    public static String LOGIN =  API_BASE + "/Users/login";
+    public static String LOGIN =  API_BASE + "/SoaUsers/login";
     public static String REGISTER =  API_BASE + "/SoaUsers";
     public static String SPORTS =  API_BASE + "/Sports";
 }
