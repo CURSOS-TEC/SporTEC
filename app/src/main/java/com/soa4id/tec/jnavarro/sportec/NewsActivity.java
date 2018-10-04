98e7df2e7d101874867f2777a431d407b076911a
Juan Navarro <jnavcamacho@gmail.com>
2018-10-04 14:00:36 -0600

    add, view of article

--- app/src/main/java/com/soa4id/tec/jnavarro/sportec/NewsActivity.java
+++ app/src/main/java/com/soa4id/tec/jnavarro/sportec/NewsActivity.java
@@ -2,12 +2,31 @@ package com.soa4id.tec.jnavarro.sportec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class NewsActivity extends AppCompatActivity {
+    private JsonObject mArticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        try{
+            String jsonString = getIntent().getStringExtra("article");
+            if (jsonString != null){
+                Log.i("JSON Article view",jsonString);
+                JsonParser parser = new JsonParser();
+                mArticle = parser.parse(jsonString).getAsJsonObject();
+            }else {
+                Log.i("JSON Article view","Null Article");
+            }
+        }
+        catch(Exception e){
+            Log.i("JSON Article",e.getMessage());
+        }
     }
 }
--- app/src/main/java/sport/NewsItemAdapter.java
+++ app/src/main/java/sport/NewsItemAdapter.java
@@ -1,15 +1,22 @@
 package sport;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.JsonObject;
+import com.soa4id.tec.jnavarro.sportec.NewsActivity;
 import com.soa4id.tec.jnavarro.sportec.R;
+import com.soa4id.tec.jnavarro.sportec.RegisterActivity;
+import com.soa4id.tec.jnavarro.sportec.UserProfileActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -20,12 +27,15 @@ public class NewsItemAdapter extends RecyclerView.Adapter<NewsItemAdapter.ViewHo
         public ImageView mImageView;
         public TextView  mTitle;
         public TextView  mShortDescription;
+        public CardView mCardView;
 
         public ViewHolder(View newsView){
             super(newsView);
             mTitle = (TextView) itemView.findViewById(R.id.textItemHead);
             mShortDescription = (TextView) itemView.findViewById(R.id.textItemDescription);
             mImageView = (ImageView) itemView.findViewById(R.id.cardview_image);
+            mCardView = itemView.findViewById(R.id.card_news);
+
         }
 
     }
@@ -52,7 +62,7 @@ public class NewsItemAdapter extends RecyclerView.Adapter<NewsItemAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        NewsItem item = mNewsList.get(position);
+        final NewsItem item = mNewsList.get(position);
         holder.mTitle.setText(item.getmTitle());
         holder.mShortDescription.setText(item.getmShortDescription());
         /*Picasso*/
@@ -61,6 +71,27 @@ public class NewsItemAdapter extends RecyclerView.Adapter<NewsItemAdapter.ViewHo
                 .resize(100, 100)
                 .centerCrop()
                 .into(holder.mImageView);
+        holder.mCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("JSON Card", "intent");
+                JsonObject article = new JsonObject();
+                article.addProperty("title" ,item.getmTitle());
+                article.addProperty("description" ,item.getmDescription());
+                article.addProperty("photoUri",item.getmURIphoto());
+                try{
+                    Intent intent = new Intent(mContext,NewsActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("article",article.toString());
+                    mContext.startActivity(intent);
+                }
+                catch (Exception e){
+                    Log.i("JSON Article Intent", e.getMessage());
+                }
+            }
+        });
+
+
 
     }
 
